49b487b57bb44b7a9e3ee225eb43c58e9d88b159
Jood80 <shawar.nujood@gmail.com>
2022-03-31 20:52:29 +0300

    chore(query): get currencies rates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  rates: any[];
+  isLoading: true;
+  error: any;
+  constructor(private apollo: Apollo) {}
 
-  constructor() {}
-
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((res: any) => {
+        this.rates = res?.data?.rates;
+        this.isLoading = res.loading;
+        this.error = res.error;
+      });
+  }
 }
